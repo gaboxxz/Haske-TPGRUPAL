01470c1c10aed2d64a3a0f410bebdfe4d999bdf5
gaboxxz <gaboxxz@gmail.com>
2016-04-16 14:43:34 -0300

    punto 2
    hice

--- TP GRUPAL.hs
+++ TP GRUPAL.hs
@@ -10,7 +10,11 @@ dameTamañoDelArchivo :: Archivo -> Int
 dameTamañoDelArchivo (Archivo _ contenido) =  (*8)(length (contenido))
 
 --Punto2)
-PRUEBA COMMIT
- modificacion desde la pagina
+archivoEsVacio = (==0).dameTamañoDelArchivo
+
+--Punto3) 
+--Para saber la cantidad de lineas de un archivo contamos la cantidad de saltos \nombre
+
+
 
 
