d8d09eaf17d56169a9e5ec26955cdeab95e34d3a
gaboxxz <gaboxxz@gmail.com>
2016-04-16 16:28:41 -0300

    punto 4
    realizamos punto 4 sin funcion isSpace

--- TP GRUPAL.hs
+++ TP GRUPAL.hs
@@ -1,9 +1,9 @@
 data Archivo = Archivo { nombre :: String, contenido :: String } deriving (Read, Show, Eq)
 
 archivoPrueba :: Archivo
-archivoPrueba = Archivo "Ejemplo" "Trabajo practico de Paradigmas\n Nombre del TP HIT" 
+archivoPrueba = Archivo "Ejemplo" "Trabajo practico de Paradigmas\n Nombre del TP HIT \n \t \f \n otralinea" 
 
-isSpace caracter = (==) caracter ' ' || (==) caracter '\n' || (==) caracter '\t' || (==) caracter '\r' || (==) caracter '\f' || (==) caracter '\v'
+isSpace caracter = (==) caracter " " || (==) caracter "\n" || (==) caracter "\t" || (==) caracter "\r" || (==) caracter "\f" || (==) caracter "\v"
 
 --Punto 1)
 dameTamañoDelArchivo :: Archivo -> Int
@@ -18,7 +18,7 @@ archivoEsVacio = (==0).dameTamañoDelArchivo
 cantidadDeLineasArch (Archivo _ contenido) = length (lines (contenido)) 
 
 --punto4)
-
+algunaLineaBlanco (Archivo _ contenido) = any (==0)(map length(map words(lines  contenido)))
 
 
 
